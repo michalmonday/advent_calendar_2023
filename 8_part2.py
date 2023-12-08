d014ea6a72cbfed806fc2890f1dd90d8285ba864
michalmonday <michalmonday17@gmail.com>
2023-12-08 17:04:16 +0000

    Day 8 solution

--- 8_part2.py
+++ 8_part2.py
@@ -1,4 +1,7 @@
 import re
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 with open('inputs/input_8.txt') as f:
     lines = [line.strip() for line in f.readlines()]
@@ -15,15 +18,23 @@ for line in lines[2:]:
 # current_location = list(locations.keys())[0]
 # destination = list(locations.keys())[-1]
 current_locations = [loc for loc in locations if loc[2] == 'A']
+initial_locations = current_locations.copy()
+print(f'Current locations = {current_locations}')
+
+first_matches = {loc : 0 for loc in current_locations}
 total = 0
-all_locs_end_with_z = False
-while not all_locs_end_with_z:
-    dir = dirs[total % len(dirs)]
+while True:
+    dir_ = dirs[total % len(dirs)]
     for i in range(len(current_locations)):
-        dir_ind = 1 if dir == 'R' else 0
+        dir_ind = 1 if dir_ == 'R' else 0
         current_locations[i] = locations[current_locations[i]][dir_ind]
-    all_locs_end_with_z = all([loc[2] == 'Z' for loc in current_locations])
     total += 1
-    if total % 1000000 == 0:
-        print(total)
-print(total)
+    for i, loc in enumerate(current_locations):
+        if loc[2] == 'Z' and first_matches[initial_locations[i]] == 0:
+            first_matches[initial_locations[i]] = total
+    if all(first_matches[loc] > 0 for loc in initial_locations):
+        print(f'All locations have matched at least once after {total} iterations')
+        break
+
+lcm = math.lcm(*list(first_matches.values()))
+print(lcm)
